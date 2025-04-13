c2698976246187775c8a7485906287f2f4e7b866
Moroni Perez <66393582+Aminadi2296@users.noreply.github.com>
2025-04-12 23:37:18 -0400

    Refactor vite.config.js for improved clarity and consistency in comments

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,14 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  root: ".",  // Root is the project root (this is already the default)
+  root: ".",  
   build: {
     outDir: "dist",
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "index.html"),  // Main entry point for index.html
-        genre: resolve(__dirname, "genre.html"),  // Genre HTML now in the root directory
+        main: resolve(__dirname, "index.html"),  
+        genre: resolve(__dirname, "genre.html"),  
+        favorites: resolve(__dirname, "favorites.html"),  
       },
     },
   },
@@ -19,11 +20,11 @@ export default defineConfig({
   },
   server: {
     proxy: {
-      // Proxy requests to Spoonacular API
+
       '/api': {
-        target: 'https://api.spoonacular.com', // The Spoonacular API URL
-        changeOrigin: true,  // Make the request appear as coming from the same origin
-        rewrite: (path) => path.replace(/^\/api/, ''), // Remove /api from the request path
+        target: 'https://api.spoonacular.com', 
+        changeOrigin: true,  
+        rewrite: (path) => path.replace(/^\/api/, ''), 
       },
     },
   },
